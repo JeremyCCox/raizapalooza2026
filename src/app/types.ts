695934a25d7e3044d28dad7274a3e424e9cb2226
JeremyCCox <93951833+JeremyCCox@users.noreply.github.com>
2026-03-30 16:44:13 -0600

    feat: fix cleanup types

--- src/app/types.ts
+++ src/app/types.ts
@@ -1,19 +1,16 @@
 export type ImageDetails = {
     src: string;
     alt: string;
-    actions?:Actions[];
+    actions?:InfoButton[];
     action?:()=>void;
     actionableText?:string;
     link?:"https://docs.google.com/forms/d/e/1FAIpQLSfoRcM9-74oSiY-hH5MCzUv4IUZA-TqsiBBCyRNO5dabWN5bQ/viewform?usp=dialog",
 }
 
-export type Actions = InfoButton | InfoLink
 
 export type InfoButton = {
-    action:({...args})=>void;
+    action?:({...args})=>void;
     actionText?:string;
-}
-export type InfoLink = {
-    link:string,
-    actionText:string,
+    link?:string,
+
 }
